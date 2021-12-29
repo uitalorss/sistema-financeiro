0854c423833331652f866d1f0a93c336ef0f048b
Uítalo <uitalo.urss@gmail.com>
2021-12-28 23:50:18 -0300

    funcionalidade para tratar os valores como negativo/positivo e como moeda e sua regiao

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ const transactions = [
   {
     id: 2,
     description: "Criação de website",
-    amount: 200000,
+    amount: 800001,
     date: "01/01/2022"
   },
   {
@@ -48,13 +48,15 @@ const treatmentTransaction = {
   addTransaction(transaction, index){
     const tr = document.createElement('tr');
     tr.innerHTML = treatmentTransaction.transactionHTML(transaction);
-    treatmentTransaction.transactionsContainer.appendChild(tr)
+    treatmentTransaction.transactionsContainer.appendChild(tr);
+    
   },
   transactionHTML(transaction){
     const cssClass = transaction.amount > 0 ? "income" : "expense";
+    const amount = utils. formatCurrency(transaction.amount);
     const tableRowTransaction = `
       <td class="description">${transaction.description}</td>
-      <td class="${cssClass}">R$ ${transaction.amount}</td>
+      <td class="${cssClass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td><img src="assets/img/minus.svg" alt="remover transação"></td>
     `
@@ -63,6 +65,21 @@ const treatmentTransaction = {
 }
 
 
+//tratamento para receber o valor, tratar os negativos e tratar como moeda e a região da moeda.
+const utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? '-' : '';
+    value = String(value).replace(/\D/g, " ")
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value    
+  }
+}
+
+//tratamento para percorrer o array
 transactions.forEach((transaction) =>{
   treatmentTransaction.addTransaction(transaction)
 })
