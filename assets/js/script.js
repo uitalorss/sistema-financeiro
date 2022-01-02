01df84c9151424b3e744fbd12e6c636e9dd658f0
Uítalo <uitalo.urss@gmail.com>
2022-01-01 22:51:37 -0300

    tratamento para mostrar na tela de balanço as entradas, as saídas e o total

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,26 +31,44 @@ const transactions = [
 
 const treatmentBalance = {
   incomes(){
-
+    let income = 0
+    transactions.forEach(transaction => {
+      if(transaction.amount > 0){
+        income += transaction.amount;
+      }
+    })
+    return income;
   },
   expenses(){
-
+    let expense = 0
+    transactions.forEach(transaction => {
+      if(transaction.amount < 0){
+        expense += transaction.amount;
+      }
+    })
+    return expense;
   },
   total(){
+    //receitas - dispesas
+    let total = treatmentBalance.incomes() - (-treatmentBalance.expenses());
 
+    return total;
   }
 }
-
 
 const treatmentTransaction = {
   transactionsContainer: document.querySelector('#data-table tbody'),
   
+  //método para adicionar a linha na tabela.
   addTransaction(transaction, index){
     const tr = document.createElement('tr');
     tr.innerHTML = treatmentTransaction.transactionHTML(transaction);
-    treatmentTransaction.transactionsContainer.appendChild(tr);
-    
+    treatmentTransaction.transactionsContainer.appendChild(tr);    
   },
+
+  //método para preenchimento das linhas da tabela de transações
+  //a depender se for despesa ou receita, a const css class vai atribuir a cor da linha
+  // o tratamento dos valores está na const amount, que recebe a função utils
   transactionHTML(transaction){
     const cssClass = transaction.amount > 0 ? "income" : "expense";
     const amount = utils. formatCurrency(transaction.amount);
@@ -61,9 +79,20 @@ const treatmentTransaction = {
       <td><img src="assets/img/minus.svg" alt="remover transação"></td>
     `
     return tableRowTransaction;
+  },
+
+  updateBalance(){
+    document
+      .getElementById("incomeDisplay")
+      .innerHTML = utils.formatCurrency(treatmentBalance.incomes()),
+    document
+      .getElementById("expenseDisplay")
+      .innerHTML = utils.formatCurrency(treatmentBalance.expenses()),
+    document
+      .getElementById("totalDisplay")
+      .innerHTML = utils.formatCurrency(treatmentBalance.total())
   }
 }
-
 
 //tratamento para receber o valor, tratar os negativos e tratar como moeda e a região da moeda.
 const utils = {
@@ -83,3 +112,5 @@ const utils = {
 transactions.forEach((transaction) =>{
   treatmentTransaction.addTransaction(transaction)
 })
+
+treatmentTransaction.updateBalance();
